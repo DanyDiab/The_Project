cffdcde3b9077321c330822cd4e26c9f801f954a
Farzin <farzin.ahmadi@hotmail.com>
2023-12-25 16:07:00 -0400

    Created enemy class that dies with projectile collision

---
+++ The_Project/src/main/java/Melee.java
@@ -0,0 +1,23 @@
+
+import static com.raylib.Raylib.*;
+import static com.raylib.Jaylib.*;
+public class Melee {
+    private int attackSpeed;
+    private int range;
+    private int damage;
+    private int posX;
+    private int posY;
+
+
+    public Melee(int attackSpeed, int range, int damage, int posX, int posY){
+        this.attackSpeed = attackSpeed;
+        this.range = range;
+        this.damage = damage;
+        this.posX = posX;
+        this.posY = posY;
+    }
+
+    public void attack(){
+        DrawLine(this.posX,this.posY,this.posX,this.posY+range,RED);
+    }
+}
