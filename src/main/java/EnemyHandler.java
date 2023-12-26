47225c11c9adb3cff6f1a6bab736d110ac6edac1
DanyDiab <danydiab04@gmail.com>
2023-12-26 01:56:22 -0400

    Cant trust ethan pt2

---
+++ src/main/java/EnemyHandler.java
@@ -0,0 +1,16 @@
+import java.util.*;
+//UNFINISHED
+public class EnemyHandler {
+    private static ArrayList<Enemy> enemyList = new ArrayList<>();
+
+
+    public EnemyHandler() {
+
+    }
+    public void spawnEnemies(){
+        int numEnemiesToSpawn = 10;
+        for(int i = 0; i < numEnemiesToSpawn; i++){
+
+        }
+    }
+}
--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,20 +1,33 @@
 import java.util.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 import static com.raylib.Raylib.*;
 import static com.raylib.Jaylib.*;
 
 public class Player extends Creature{
     private int projAngle;
     private static ArrayList<Projectile> projList = new ArrayList<>();
+    private Melee sword = new Melee(5,100,posX, posY, posY);
+    private boolean canMelee = true;
+    private static final long MELEE_COOLDOWN = 1000;
+    private long lastMeleeTime = 0;
+    private boolean canShoot = true;
+    private static final long SHOT_COOLDOWN = 500;
+    private long lastShotTime = 0;
+
+
 
     public Player() {
         super(100,10, 10, 100, 100, 5, 20);
         DrawCircle(posX, posY, size, RED);
     }
-//
 //move function that updates player posistions and redraws the position.
     public void move() {
         if (IsKeyDown(KEY_W) && posY > 3 + size) {
             posY -= moveSpeed;
+
         }
         if (IsKeyDown(KEY_S) && posY < GetScreenHeight() - size) {
             posY += moveSpeed;
@@ -26,13 +39,69 @@ public class Player extends Creature{
             posX -= moveSpeed;
         }
     }
-    public void createProjecitle(){
-        if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
-            DrawCircle(posX, posY, 10, PURPLE);
-            setProjecitleDirection();
-            projList.add(new Projectile(10,posX,posY,10, projAngle));
+    public void createProjecitle() {
+        if (IsMouseButtonDown(MOUSE_BUTTON_LEFT) && canShoot()) {
+            if (canShoot) {
+                DrawCircle(posX, posY, 10, PURPLE);
+                setProjecitleDirection();
+                projList.add(new Projectile(10, posX, posY, 10, projAngle));
+                resetShotCooldown();
+            }
         }
         checkProjectilesBounds();
+    }
+
+    private boolean canShoot() {
+        return canShoot;
+    }
+
+    private void resetShotCooldown() {
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+
+        executor.submit(() -> {
+            try {
+                TimeUnit.MILLISECONDS.sleep(SHOT_COOLDOWN);
+            } catch (InterruptedException e) {
+                System.out.println("got interrupted!");
+            }
+            canShoot = true;
+            executor.shutdown();
+        });
+
+        // Update the last shot time immediately to prevent rapid clicks during cooldown
+        lastShotTime = System.currentTimeMillis();
+        canShoot = false;
+    }
+
+
+    public void melee() {
+        if (IsKeyDown(KEY_SPACE) && canMelee()) {
+            if (canMelee) {
+                sword.setPosX(posX);
+                sword.setPosY(posY);
+                sword.attack();
+                canMelee = false;
+                lastMeleeTime = System.currentTimeMillis();
+                resetMeleeCooldown();
+            }
+        }
+    }
+    private boolean canMelee() {
+        long currentTime = System.currentTimeMillis();
+        return (currentTime - lastMeleeTime) >= MELEE_COOLDOWN;
+    }
+    private void resetMeleeCooldown() {
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+
+        executor.submit(() -> {
+            try {
+                TimeUnit.MILLISECONDS.sleep(MELEE_COOLDOWN);
+            } catch (InterruptedException e) {
+                System.out.println("got interrupted!");
+            }
+            canMelee = true;
+            executor.shutdown();
+        });
     }
     public void checkProjectilesBounds(){
         for(int i = 0; i < projList.size(); i++){
@@ -76,7 +145,13 @@ public class Player extends Creature{
     public void update(){
         move();
         createProjecitle();
+        melee();
+        sword.update();
         DrawCircle(posX, posY, size, RED);
+    }
+
+    public static ArrayList<Projectile> getProjList() {
+        return projList;
     }
 }
 
